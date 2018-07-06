74a687dd9aac2696e9ec74ec7453224401980327
Noer21 <noer@yahoo.com>
2018-07-06 10:51:58 -0700

    guest mode 50%

--- src/screens/guest/styles.js
+++ src/screens/guest/styles.js
@@ -13,43 +13,11 @@ const styles = StyleSheet.create({
         zIndex: 1,
         position: 'absolute'
     },
-    Images:{
-        height: 20,
-        alignSelf: 'center',
-        resizeMode: 'contain',
-    },
-    bayarDesc: {
-        textAlign: 'center',
-        fontSize: 15,
-        margin: 10,
-        color: 'grey',
-    },
-    bayarNum: {
-        textAlign: 'center',
-        fontSize: 25,
-        fontWeight: 'bold',
-        margin: 10,
-        marginTop: 0,
-        paddingBottom: 10,
-        borderBottomWidth: 0.5,
-    },
-    uploadButton: {
-        marginTop: 20,
-        borderRadius:80,
-        borderColor:'white',
-        borderWidth:2, 
-        backgroundColor: '#007300'
-    },
-    confirmButton: {
-        height : 60,
-        ...StyleSheet.absoluteFillObject,
-        backgroundColor: '#007300',
-        justifyContent: 'center',
-    },
-    ButtonWord: {
-        fontSize: 17,
-        fontWeight: 'bold'
-    },
+    footer: {
+        backgroundColor: 'rgba(0, 115, 0, 0.7)',
+        height: 50,
+        width: '100%'
+    }
 });
 
 export default styles;
